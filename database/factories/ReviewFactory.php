22fd3c336271ff2e3e10aed506b43017ccf312ec
fumbuka1998 <limbufumbuka@gmail.com>
2022-09-20 14:52:48 +0300

    my first commit

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Model\Product;
+use Faker\Generator as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'product_id' => function(){
+                return Product::all()->random();	
+            },
+            'customer' => $faker->name,
+            'review' => $faker->paragraph,
+            'star' => $faker->numberBetween(0,5)
+        ];
+    }
+}
